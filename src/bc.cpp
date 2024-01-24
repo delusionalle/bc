49c2c7c05436937422f829ad5856d27f3ab0ed23
delusionalle <antiiiclimactical@gmail.com>
2024-01-24 10:58:40 +0600

    fix: buildfile parsing

--- src/bc.cpp
+++ src/bc.cpp
@@ -1,22 +1,26 @@
 #include "base.hpp"
 #include "rw/file.hpp"
+#include "sup/toml.hpp"
 
 constexpr const char* BC_HELP_MSG =
-R"(    bc - simple c/c++ build system
+        R"(    bc - simple c/c++ build system
     |- <no args>        -> run build.toml config in debug mode
     |- -[d|r]           -> run build.toml in selected mode
 
-    -   build.toml description
+    build.toml description
     |- [sources.src] - project code files to be included in
     |- [] -
 )";
 
 int main(int argc, char** argv) {
-    if (argc > 1 && strcmp(argv[1], "help") == 0) {
+    if (argc == 2 && strcmp(argv[1], "help") == 0) {
         std::cout << BC_HELP_MSG << '\n';
         return 0;
+    } else if (argc == 2 && strcmp(argv[1], "parse") == 0) {
+        auto opts = bc::read_buildfile("build.toml");
+        std::cout << opts.output_path << '\n';
     }
 
-    auto opts = bc::read_buildfile("build.toml");
-    std::cout << "main bc::read_buildfile output_dir = " << opts.output_path << '\n';
+    // auto opts = bc::read_buildfile("build.toml");
+    // std::cout << "main bc::read_buildfile output_dir = " << opts.output_path << '\n';
 }
--- src/rw/file.cpp
+++ src/rw/file.cpp
@@ -2,8 +2,15 @@
 #include "sup/toml.hpp"
 
 bc::Opts bc::read_buildfile(std::string_view filename = "build.toml") {
-    auto buildfile = toml::parse(filename);
     Opts opts{};
 
-    opts.output_path = buildfile["src"]["output_dir"].value_or("");
+    try {
+        toml::table config = toml::parse_file(filename);
+
+        opts.output_path = config["src"]["output_dir"].value_or("");
+    } catch (const toml::parse_error& err) {
+        std::cerr << "ERR @bc::read_buildfile -> [toml::parse_error] " << err << '\n';
+    }
+
+    return opts;
 }
